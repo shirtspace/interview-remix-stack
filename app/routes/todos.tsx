59b2135fcbe465114d399f824ef8c224efb322b9
Travis Saunders <travis@shirtspace.com>
2025-01-14 10:01:13 -0800

    Add todos route

---
+++ app/routes/todos.tsx
@@ -0,0 +1,11 @@
+const Todos: React.FC = () => {
+  return (
+    <div className="flex flex-col mx-auto max-w-[420px]">
+      <h1 className="text-3xl font-bold mt-10 text-center">
+        Todos
+      </h1>
+    </div>
+  )
+}
+
+export default Todos;
